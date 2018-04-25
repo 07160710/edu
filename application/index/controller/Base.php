963d045f4b71c80be13fcaf61ce013f79c2b4a3d
huangkaifeng <1195745286@qq.com>
2018-04-25 20:38:28 +0800

    first to commit,and this is a school system

---
+++ application/index/controller/Base.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/4/25
+ * Time: 20:09
+ */
+
+namespace app\index\controller;
+
+
+use think\Controller;
+
+class Base extends Controller
+{
+
+}
