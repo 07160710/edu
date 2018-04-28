eb56a17078665283b6c6277e465881e3e561574c
huangkaifeng <1195745286@qq.com>
2018-04-28 23:15:35 +0800

    完成用户登录验证

--- application/index/controller/User.php
+++ application/index/controller/User.php
@@ -9,14 +9,58 @@
 namespace app\index\controller;
 
 use app\index\controller\Base;
+use think\Request;
+use app\index\model\User as UserModel;
+
 class User extends Base
 {
     public function login()
     {
         return $this -> view -> fetch();
     }
-    public function checkLogin()
+    public function checkLogin(Request $request)
     {
+
+        $status = 0;
+        $result = '';
+        $data = $request ->param();
+
+
+        $rule = [
+            'username|用户名' => 'require',
+            'password|密码' => 'require',
+            'verify|验证码' => 'require|captcha',
+        ];
+        $msg = [
+            'username' => ['require' => '用户名不能为空,请检查'],
+            'password' => ['require' => '密码不能为空,请检查'],
+            'verify' =>[
+                'require' => '验证码不能为空,请检查',
+                'captcha' => '验证码错误',
+            ]
+        ];
+        $result = $this->validate($data,$rule,$msg);
+        if($result === true){
+            $map = [
+                'username' => $data['username'],
+                'password' => md5($data['password']),
+            ];
+            $user = UserModel::get($map);
+            if($user == null){
+                $result = '没有找到该用户';
+            }else{
+                $status = 1;
+                $result = '验证通过,点击[确定]后进入后台';
+            }
+        }
+
+        return ['status'=>$status,'message'=>$result,'data'=>$data];
+
+
+
+
+        //var_dump($result);
+
     }
     public function logout()
     {
---
+++ application/index/model/User.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/4/28
+ * Time: 22:43
+ */
+namespace app\index\model;
+use think\Model;
+
+class User extends Model
+{
+
+}
